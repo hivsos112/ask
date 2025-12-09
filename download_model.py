847154877dd3faddfa7cb7ce919332d151bacc9a
YangLi <yangli@YangLideMac-mini.local>
2025-12-09 23:14:59 +0800

    Fix Dockerfile: Debug module import error by listing installed packages

--- download_model.py
+++ download_model.py
@@ -6,10 +6,17 @@ def main():
     try:
         print('Checking imports...', flush=True)
         
+        # cv2 package name is opencv-python-headless
         print('Importing cv2...', flush=True)
-        import cv2
-        print(f'cv2 imported successfully: {cv2.__version__}', flush=True)
-        
+        try:
+            import cv2
+            print(f'cv2 imported successfully: {cv2.__version__}', flush=True)
+        except ImportError:
+            print("ImportError: cv2 not found. Trying to find installed packages...", flush=True)
+            import subprocess
+            subprocess.check_call([sys.executable, "-m", "pip", "list"])
+            raise
+
         print('Importing onnxruntime...', flush=True)
         import onnxruntime
         print(f'onnxruntime imported successfully: {onnxruntime.__version__}', flush=True)
